45c04c4d47e2afd4ba4d7d0c034c43065f2be6e5
JefferyJeffery <jeffery@rinse.io>
2014-08-20 12:53:22 +0800

    init project

---
+++ iOS/JYGetUserID_Facebook_Twitter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JYGetUserID_Facebook_Twitter
+//
+//  Created by Jeffery on 2014/8/20.
+//  Copyright (c) 2014年 Jeffery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS/JYGetUserID_Facebook_Twitter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JYGetUserID_Facebook_Twitter
+//
+//  Created by Jeffery on 2014/8/20.
+//  Copyright (c) 2014年 Jeffery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
